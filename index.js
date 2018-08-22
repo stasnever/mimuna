9a062da80f25b19e1b2303e1950f7b8cc5d61775
stasn <stas@moonsite.co.il>
2018-08-22 13:54:04 +0300

    polyfill added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from "react-native";
 import Root from "./src/routes/Root";
 
+if (!global._babelPolyfill) {
+  require("babel-polyfill");
+}
 AppRegistry.registerComponent("MY", () => Root);
