92b67eb93ae3fa40bfae0a0440edf6afebd37fec
stasn <stas@moonsite.co.il>
2018-08-22 14:21:34 +0300

    polyfiil fix

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
+import "babel-polyfill";
 import { AppRegistry } from "react-native";
 import Root from "./src/routes/Root";
 
-if (!global._babelPolyfill) {
-  require("babel-polyfill");
-}
 AppRegistry.registerComponent("MY", () => Root);
